4aa571778a92305c3ef72df09495d0d5da762915
lucabDev <luccacpm@gmail.com>
2023-04-17 15:46:50 -0300

    feat: client

---
+++ src/Jabank/Cliente.java
@@ -0,0 +1,28 @@
+package Jabank;
+
+/* Recomendação é manter privado */
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String profissao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getProfisao() {
+        return profissao;
+    }
+}
